69b5aaf190a85b5a17ba5a7e728a69c8b41205d7
Changhoon Ji <cji2@gmu.edu>
2020-01-13 19:39:36 -0500

    modify CustomerController with InitBinder method to trim whitespace

--- spring-project-mvc/src/edu/gmu/springmvc/CustomerController.java
+++ spring-project-mvc/src/edu/gmu/springmvc/CustomerController.java
@@ -2,15 +2,33 @@ package edu.gmu.springmvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	/* the following is for pre-processing every String form data.
+	 * it removes leading and trailing white space.
+	 * if String only has white spaces, it trims it to null.
+	 * @InitBinder: pre-process all web requests coming into our Controller. */
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+
+		/* StringTrimmerEditor is defined in Spring API.
+		 * (its argument is null, which means trimming to null) */
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		// it will be registered as customized editor, so it works for every string class.
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model aModel) {
