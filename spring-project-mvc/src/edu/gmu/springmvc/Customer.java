cb5c6b030c2bb05efcb198a0481fd6afafcf744d
Changhoon Ji <cji2@gmu.edu>
2020-01-13 23:22:35 -0500

    modify Customer class to add form validation rule

--- spring-project-mvc/src/edu/gmu/springmvc/Customer.java
+++ spring-project-mvc/src/edu/gmu/springmvc/Customer.java
@@ -3,6 +3,8 @@ package edu.gmu.springmvc;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import edu.gmu.springmvc.validation.CourseCode;
+
 public class Customer {
 	
 	private String firstName;
@@ -10,6 +12,18 @@ public class Customer {
 	@NotNull(message="is required")
 	@Size(min=2, message="is required")
 	private String lastName;
+	
+//	@CourseCode
+	@CourseCode(value="GMU", message="should start with GMU")
+	private String courseCode;
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
 
 	public String getFirstName() {
 		return firstName;
